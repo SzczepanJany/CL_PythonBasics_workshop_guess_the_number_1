ce256873a7a22f879ecc5362adaa3d840aff2bc5
SzczepanJany <szczepan.jany@gmail.com>
2021-02-09 21:56:53 +0100

    Added functions descriptions

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from random import randint
 
-def pick_a_number():
-    result = randint(1,100)
+def pick_a_number(min_value, max_value ):
+    """
+    Function is choosing random number from a given range
+    """
+    result = randint(min_value,max_value)
     return result
 
 def ask_for_answer():
+    """
+    Function is asking user for an answer and checkin if it is a number 
+    """
     while True:
         user_answer = input("Guess the number: ")
         try:
@@ -15,6 +21,9 @@ def ask_for_answer():
     return user_answer
 
 def check_answer(user_imput, computer_imput):
+    """
+    Function is comparing user answer with computer picked number
+    """
     if user_imput < computer_imput:
         print("To small!")
         return False
@@ -26,7 +35,7 @@ def check_answer(user_imput, computer_imput):
         return True
 
 
-computer_picked_number = pick_a_number()
+computer_picked_number = pick_a_number(1,100)
 win_status = False
 
 while win_status == False:
